deb89e9459ef273a5c133e96e2291fb196f43334
Carlos Henrique Alves Morais <166178484+carlosmorais01@users.noreply.github.com>
2025-06-20 17:31:22 -0300

    Revert "Adicionando novos atributos e métodos relacionados à locação em entidades de veículos e implementando `Serializable` em `Veiculo`"
    This reverts commit 0904257ac1deea455c0aebf7459f236e15d6f0cc.

--- src/entities/Moto.java
+++ src/entities/Moto.java
@@ -10,41 +10,10 @@ public class Moto extends Veiculo {
     private boolean portaCarga;
     private int raioPneu;
 
-    public Moto(String descricao, String placa, String marca, String nome, String modelo, int ano, Cor cor, Funcao funcao, double quilometragem, int numeroPassageiros, double consumoCombustivelPLitro, double velocidadeMax, boolean automatico, Combustivel combustivel, Tracao tracao, int quantAssento, boolean airBag, String caminhoFoto, double potencia, boolean vidroEletrico, boolean arCondicionado, boolean multimidia, boolean entradaUSB, boolean vidroFume, double peso, boolean engate, boolean direcaoHidraulica, double valorDiario, int cilindradas, boolean portaCarga, int raioPneu, int locacoes, boolean locado) {
-        super(descricao, placa, marca, nome, modelo, ano, cor, funcao, quilometragem, numeroPassageiros, consumoCombustivelPLitro, velocidadeMax, automatico, combustivel, tracao, quantAssento, airBag, caminhoFoto, potencia, vidroEletrico, arCondicionado, multimidia, entradaUSB, vidroFume, peso, engate, direcaoHidraulica, valorDiario, locacoes, locado);
-        this.cilindradas = cilindradas;
-        this.portaCarga = portaCarga;
-        this.raioPneu = raioPneu;
-    }
-
     public Moto(String descricao, String placa, String marca, String nome, String modelo, int ano, Cor cor, Funcao funcao, double quilometragem, int numeroPassageiros, double consumoCombustivelPLitro, double velocidadeMax, boolean automatico, Combustivel combustivel, Tracao tracao, int quantAssento, boolean airBag, String caminhoFoto, double potencia, boolean vidroEletrico, boolean arCondicionado, boolean multimidia, boolean entradaUSB, boolean vidroFume, double peso, boolean engate, boolean direcaoHidraulica, double valorDiario, int cilindradas, boolean portaCarga, int raioPneu) {
         super(descricao, placa, marca, nome, modelo, ano, cor, funcao, quilometragem, numeroPassageiros, consumoCombustivelPLitro, velocidadeMax, automatico, combustivel, tracao, quantAssento, airBag, caminhoFoto, potencia, vidroEletrico, arCondicionado, multimidia, entradaUSB, vidroFume, peso, engate, direcaoHidraulica, valorDiario);
         this.cilindradas = cilindradas;
         this.portaCarga = portaCarga;
-        this.raioPneu = raioPneu;
-    }
-
-    public int getCilindradas() {
-        return cilindradas;
-    }
-
-    public void setCilindradas(int cilindradas) {
-        this.cilindradas = cilindradas;
-    }
-
-    public boolean isPortaCarga() {
-        return portaCarga;
-    }
-
-    public void setPortaCarga(boolean portaCarga) {
-        this.portaCarga = portaCarga;
-    }
-
-    public int getRaioPneu() {
-        return raioPneu;
-    }
-
-    public void setRaioPneu(int raioPneu) {
         this.raioPneu = raioPneu;
     }
 }
