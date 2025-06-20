a6f4ad03371d6c38a9b9ccbd82f23395adffef04
Carlos Henrique Alves Morais <166178484+carlosmorais01@users.noreply.github.com>
2025-06-20 17:31:22 -0300

    Revert "Renomeando método de `removerSaldo` para `descontarSaldo` em `Cliente`"
    This reverts commit 0677558f8427d3e5f8fb46338dc8f986fd2852f6.

--- src/entities/Cliente.java
+++ src/entities/Cliente.java
@@ -16,7 +16,7 @@ public class Cliente extends Pessoa {
         this.saldo += valor;
     }
 
-    public void descontarSaldo(double valor) {
+    public void removerSaldo(double valor) {
         this.saldo -= valor;
     }
 
